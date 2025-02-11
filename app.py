29cb8e2d23fda7ac3e2e83eea4488334f0134e7d
Ryzhon <ryuta.hokazono@gmail.com>
2025-02-11 13:05:26 +0900

    refactor: デプロイに必要な変更を追加する

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import openai
 import os
 from dotenv import load_dotenv
 
-
-
 load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -14,5 +12,6 @@ app = Flask(__name__)
 app.secret_key = "SECRET_KEY"
 app.register_blueprint(views_blueprint)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
